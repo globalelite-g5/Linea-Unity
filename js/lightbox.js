5eaafce35d09605806180d4b0a7947d82f89b85b
GlobalElite <71672839+globalelite-g5@users.noreply.github.com>
2020-09-28 15:43:52 -0300

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,14 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('agregar.imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen;
+}
